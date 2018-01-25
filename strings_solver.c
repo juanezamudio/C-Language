69ebc9d4923656df37270361ab202b8d827843d7
Juan Zamudio <jzamudio14@gmail.com>
2018-01-25 01:54:37 -0800

    part 1

--- strings_solver.c
+++ strings_solver.c
@@ -16,22 +16,28 @@
 #define MAX_INTS 6
 
 char first_line[14] = "Cecil Sagehen";
+char buffer[14];
+int int_array[4];
+
 double second_line;
-// char *pointer_first;
-//
-// char *pointer_first = &first_line[0];
-//
-// char *pointer_second = &first_line[1];
-char *pointer;
+char *char_pointer = NULL;
+double *doub_char_pointer = NULL;
 
 int main() {
-  // *char_pointer = (char *)malloc(sizeof(first_line));
-
+  
   for (int i = 0; i < 14; i++) {
-    char *pointer = &first_line[i];
+    char *char_pointer = &first_line[i];
 
-    printf("%c\t%d\t%x\n", *pointer, *pointer, *pointer);
+    printf("%c\t%d\t%x\t", *char_pointer, *char_pointer, *char_pointer);
+    printf("\n");
   }
+
+  for (int i = 14, j = 0; i > 0; i-=4, j++) {
+    char *char_pointer = &first_line[i];
+
+    int_array[j] = printf("%x", *char_pointer);
+  }
+
   printf("\n");
   return 0;
 }
